c5f6faf0565b4c905b4f82b10c3f6ad88e4a9881
DmitriyUshkvarok <82167806+DmitriyUshkvarok@users.noreply.github.com>
2023-05-14 20:06:46 +0000

    Deploying to gh-pages from @ DmitriyUshkvarok/social-profile@2fb9c922c1c7ff6b8cbd8bddd4f2967d99e3c686 🚀

---
+++ static/js/680.93efe654.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunksocial_profile=self.webpackChunksocial_profile||[]).push([[680],{680:function(e,i,c){c.r(i);c(2791);var l=c(184);i.default=function(){return(0,l.jsx)("div",{children:(0,l.jsx)("h1",{children:"\u043f\u0440\u0438\u0432\u0435\u0442 \u043c\u0438\u0440 \u0451\u043b\u043a\u0438 \u0437\u0435\u043b\u0451\u043d\u044b\u0435 \u0435\u0431\u0430\u043b\u0441\u044f \u0431\u044b \u0432 \u0440\u043e\u0442 \u044d\u0442\u043e\u0442 \u0444\u0430\u0435\u0440\u0431\u0435\u0439\u0441"})})}}}]);
+//# sourceMappingURL=680.93efe654.chunk.js.map
